82ea10f2448455e380a65f55183ff004919099ba
liuzhongtu <553922774@qq.com>
2021-02-22 20:39:03 +0800

    修改pom.xml文件，使得JUnit5能够运行 实现GHappyTest，完成测试，并修改GHappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,16 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+    @ParameterizedTest(name = "string={0}, happy={1}")
+    @CsvSource({"xxggxx,true", "xxgxx,false", "xxggyygxx,false","gg,true",",false","gxg,false","ggx,true","xgg,true","g,false","xxx,false"})
+    public void testAlgorithm(String string, boolean expectedResult){
+        boolean result = new GHappy().gHappy(string);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
